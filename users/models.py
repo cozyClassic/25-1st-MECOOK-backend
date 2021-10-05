6046cf24e2a5bb8f7a403e93d2aa304152226185
Your Name <kimdh1004yo@gmail.com>
2021-10-05 22:26:19 +0900

    [ADD] Implemented signup and login function
    Made database table that contains user informations. Views.py file contains
    SignupView and LoginView.
    SignupView
    -if account, password, email is valid, user information stored in database
    LoginView
    -if account, password is in the database, returns access token

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User(models.Model):
+    account      = models.CharField(max_length=100)
+    password     = models.CharField(max_length=100)
+    name         = models.CharField(max_length=100)
+    gender       = models.CharField(max_length=100)
+    nationality  = models.CharField(max_length=100)
+    birth_date   = models.DateTimeField()
+    phone_number = models.CharField(max_length=50)
+    email        = models.CharField(max_length=100)
+    created_at   = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'users'
---
+++ users/views.py
@@ -0,0 +1,67 @@
+import json, re, bcrypt, jwt
+
+from django.http  import JsonResponse
+from django.views import View
+from .models      import User
+from my_settings  import SECRET_KEY, ALGORITHM
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            account  = data['account']
+            password = data['password']
+            email    = data['email']
+
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+                return JsonResponse({'MESSAGE' : 'invalid_email_format'}, status=400)
+
+            if not re.match('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}', password):
+                return JsonResponse({'MESSAGE' : 'invalid_password_format'}, status=400)
+
+            if User.objects.filter(account = account).exists():
+                return JsonResponse({'MESSAGE': 'existing_id'}, status=400)
+            
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'MESSAGE': 'email_occupied'}, status=400)
+
+            hashed_password   = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            decoded_password  = hashed_password.decode('utf-8')
+
+            User.objects.create(
+                    account      = account,
+                    password     = decoded_password,
+                    name         = data['name'],
+                    gender       = data['gender'],
+                    nationality  = data['nationality'],
+                    birth_date   = data['birth_date'],
+                    phone_number = data['phone_number'],
+                    email        = email
+                )
+
+            return JsonResponse({'MESSAGE': 'signup_success'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'key_error'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            account  = data['account']
+            password = data['password']
+
+            if not User.objects.filter(account=account).exists():
+                return JsonResponse({'MESSAGE': 'invalid_id'}, status=400)
+            
+            user = User.objects.get(account=account)
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'MESSAGE': 'invalid_password'}, status = 400)
+            
+            access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm=ALGORITHM)
+
+            return JsonResponse({'TOKEN': access_token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'key_error'}, status=400)
