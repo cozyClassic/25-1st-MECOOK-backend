ee7a34361a890e2e77bfc0956afd5b4acff98603
KIM <nzlk112@gmail.com>
2021-10-06 19:46:20 +0900

    데이터 저장 완료

---
+++ importcsv.py
@@ -0,0 +1,95 @@
+import os
+import django
+import csv
+import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mecook.settings")
+django.setup()
+from product.models import Menus
+
+CSV_PATH_PRODUCTS = '.csv/menus.csv'
+
+with open(CSV_PATH_PRODUCTS) as in_file :
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader :
+        Menus.objects.create(
+            name = row[1])
+
+
+from product.models import Categories
+
+CSV_PATH_PRODUCTS = '.csv/category.csv'
+
+with open(CSV_PATH_PRODUCTS) as in_file :
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader :
+        Categories.objects.create(
+            name = row[1],
+            menu_id=row[2])
+
+
+
+from product.models import Products
+
+CSV_PATH_PRODUCTS = '.csv/product.csv'
+
+with open(CSV_PATH_PRODUCTS) as in_file :
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader :
+        Products.objects.create(
+            name                = row[1],
+            category_id         = row[2],
+            is_visible          = row[3],
+            origin_price_KRW    = row[4],
+            discounted_price_KRW= row[5],
+            event_price_KRW     = row[6],
+            servings_g_people   = row[7],
+            cook_time           = row[8],
+            detail_top_text     = row[9],
+            thumbnail_over_url  = row[10],
+            thumbnail_out_url   = row[11],
+            detail_HTML         = row[12],
+            priority            = row[13],
+            seller_id           = row[14],
+            )
+
+
+from product.models import ProductsMainImages
+
+CSV_PATH_PRODUCTS = '.csv/product_main_image.csv'
+
+with open(CSV_PATH_PRODUCTS) as in_file :
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader :
+        ProductsMainImages.objects.create(
+            main_image_url  = row[1],
+            product_id      = row[2]
+            )
+
+
+from product.models import Hashtags
+
+CSV_PATH_PRODUCTS = '.csv/hashtag.csv'
+
+with open(CSV_PATH_PRODUCTS) as in_file :
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader :
+        Hashtags.objects.create(
+            name = row[1])
+
+from product.models import ProductAndHashtag
+
+CSV_PATH_PRODUCTS = '.csv/product_and_hashtag.csv'
+
+with open(CSV_PATH_PRODUCTS) as in_file :
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader :
+        ProductAndHashtag.objects.create(
+            product_id = row[1],
+            hashtag_id = row[2]
+            )
