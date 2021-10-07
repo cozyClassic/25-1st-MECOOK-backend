1d6e97d3bfe2893dfdb502c76843ea46c5ed4877
KIM <nzlk112@gmail.com>
2021-10-07 16:14:12 +0900

    merge 시도2

--- product/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls    import path
-from product.views  import CategoryList, ProductList, MenuList
-
-urlpatterns = [
-    path('/category', CategoryList.as_view(), name = 'category'),
-    path('/menu', MenuList.as_view(), name = 'menu'),
-    path('/product_all', ProductList.as_view(), name='products_all'),
-]
