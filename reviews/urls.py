604eb7befa4730055c43aa673290cc63f82efca5
Your Name <kimdh1004yo@gmail.com>
2021-10-09 16:48:39 +0900

    댓글 삭제 기능 구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views      import ReviewView, GetReviewView
+from .views      import ReviewView, GetReviewView, EditReviewView
 
 urlpatterns = [
         path('/comment', ReviewView.as_view()),
-        path('/list/<int:product_id>', GetReviewView.as_view()),
-        # path('review/<int:review_id>', EditReviewView.as_view())      
+        path('/comment/<int:review_id>', EditReviewView.as_view()),
+        path('/list/<int:product_id>', GetReviewView.as_view()),      
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -46,7 +46,7 @@ class ReviewView(View):
 
 class GetReviewView(View):
 
-    def get(self, request,product_id):
+    def get(self, request, product_id):
             review_by_product = Reviews.objects.filter(product_id=product_id)
             ret = []
 
@@ -59,3 +59,17 @@ class GetReviewView(View):
                 })
 
             return JsonResponse({'review_by_product': ret}, status=201)
+
+class EditReviewView(View):
+    @login_decorator
+    def delete(self, request, review_id):
+        authorized_user = request.user
+        review = Reviews.objects.get(id=review_id)
+        user = review.user
+        if authorized_user == user:
+            review.delete()
+            return JsonResponse({'message': 'deleted'}, status=204)
+        else:
+            return JsonResponse({'message': 'not_allowed'}, status=400)
+
+        
