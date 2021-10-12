515b27e6d94fa0a25fab85bc26191c6f83b45653
Your Name <kimdh1004yo@gmail.com>
2021-10-12 15:35:00 +0900

    카트 기능 구현

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ class User(models.Model):
     account      = models.CharField(max_length=100)
     password     = models.CharField(max_length=100)
     name         = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=50)
     email        = models.CharField(max_length=100)
+    points       = models.DecimalField(decimal_places=3, max_digits=10, default="100000")
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
 
