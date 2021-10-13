3620bcaa2ffaeed1e87e1199942cd888efbee941
Your Name <kimdh1004yo@gmail.com>
2021-10-13 12:14:55 +0900

    리밋 오프셋

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from .views      import ReviewView
 
 urlpatterns = [
         path('/comment', ReviewView.as_view()),
-        path('/list/<int:review_id>', ReviewView.as_view()),
         path('/comment/<int:product_id>', ReviewView.as_view()),      
+        path('/comment/<int:review_id>', ReviewView.as_view()),
 ]
