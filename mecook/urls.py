aac3858d016789346e63f6f18a97283c04356f2c
Your Name <kimdh1004yo@gmail.com>
2021-10-13 17:10:53 +0900

    in progress

--- mecook/urls.py
+++ mecook/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('user', include('users.urls')),
     path('product', include('product.urls')),
     path('like', include('likes.urls')),
-    path('cart', include('carts.urls'))
+    path('cart', include('carts.urls')),
+    path('order', include('orders.urls'))
 ]
--- orders/views.py
+++ orders/views.py
@@ -4,15 +4,33 @@ from django.http           import JsonResponse
 from django.views          import View
 
 from .models               import *
-from product.models        import Products
 from carts.models          import Carts
+from users.models          import User
 from users.utils           import login_decorator
 
 class OrderView(View):
     @login_decorator
     def post(self, request):
         user = request.user
+        Orders.objects.create(
+            user = user,
+            order = OrderStatus.objects.get(id=1) #OrderStatus id=1일땐 주문완료
+        )
+        return JsonResponse({'message': 'success'}, status=201)
+
+    @login_decorator
+    def get(self, request):
+        user = request.user
         items = Carts.objects.filter(user=user)
 
+        total = 0
         for item in items:
-            int(item.product.origin_price_KRW)
+            a = int(item.product.origin_price_KRW)
+            b = item.quantity
+            total += a * b
+        print(total)
+
+        user.points -= total
+        user.save()
+
+        return JsonResponse({'message': 'point_off'}, status=201)
