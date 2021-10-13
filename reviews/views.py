4c679181a49b8540da224f3241744d43580bf4c1
Your Name <kimdh1004yo@gmail.com>
2021-10-13 11:43:47 +0900

    리밋 오프셋

--- reviews/views.py
+++ reviews/views.py
@@ -1,21 +1,19 @@
 import json
-from django.core import paginator
 
 from django.http           import JsonResponse
 from django.views          import View
-from django.db             import transaction
+
 from .models               import Reviews
 from product.models        import Products
 from users.utils           import login_decorator
 
 class ReviewView(View):
-    @transaction.atomic
     @login_decorator
     def post(self, request):
         try:    
             data        = json.loads(request.body)
             user        = request.user
-            product = Products.objects.get(id=data['product'])
+            product     = Products.objects.get(id=data['product'])
 
             Reviews.objects.create(
                     user    = user,
@@ -31,13 +29,12 @@ class ReviewView(View):
             return JsonResponse({'message': 'key_error'}, status=400)
 
     def get(self, request, product_id):
+        try:
+            sort   = request.GET.get('sort', '-created_at')
             offset = int(request.GET.get('offset', 0)) 
-            limit  = int(request.GET.get('limit', 0))
+            limit  = int(request.GET.get('limit', 5))
 
-            review_by_product = Reviews.objects.filter(product_id=product_id)[offset:offset+limit]
-
-            if limit-offset > 20 :
-                return JsonResponse({'message':'too much lists'}, status=400)
+            review_by_product = Reviews.objects.filter(product_id=product_id).order_by(sort)[offset:offset+limit]
 
             ret = [{'review_id': review.id,
                     'review'   : review.review,
@@ -46,13 +43,21 @@ class ReviewView(View):
 
             return JsonResponse({'review_by_product': ret}, status=201)
 
+        except Reviews.DoesNotExist:
+            return JsonResponse({'message': 'review_does_not_exits'}, status=404)
+
+        except KeyError:
+            return JsonResponse({'message': 'key_error'}, status=400)
+
     @login_decorator
     def delete(self, request, review_id):
-        authorized_user = request.user
-        review = Reviews.objects.get(id=review_id)
-        user = review.user
-        if authorized_user == user:
+        try:
+            review = Reviews.objects.get(id=review_id)
             review.delete()
             return JsonResponse({'message': 'deleted'}, status=204)
-        else:
-            return JsonResponse({'message': 'not_allowed'}, status=400)
+        
+        except Reviews.DoesNotExist:
+            return JsonResponse({'message': 'message_does_not_exist'}, status=404)
+
+        except KeyError:
+            return JsonResponse({'message': 'key_error'}, status=400)
